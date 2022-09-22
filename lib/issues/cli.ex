7ab782e2a1cd3087f43dea220683b076bb4250da
Robert <bob@rwscott.co.uk>
2022-09-22 09:30:27 +0100

    add cli_test

---
+++ lib/issues/cli.ex
@@ -0,0 +1,27 @@
+defmodule Cli do
+  @moduledoc """
+  Entry point and command line parsing
+  """
+  @default_count 4
+
+  def run(argv) do
+    parse_args(argv)
+  end
+
+  @doc """
+  argv - github  username, project name and option number of entries or -h or --help
+
+  Returns `{user, project, count}` or `:help`
+
+  """
+   def parse_args(argv) do
+    args = OptionParser.parse(argv, switches: [ help: :boolean], aliases: [h: :help ])
+
+    case args do
+      { [help: true], _, _} -> :help
+      {_, [ user, project, count ]} -> {user, project, count}
+      {_, [ user, project], _} -> {user, project, @default_count}
+      _ -> :help
+    end
+  end
+end
---
+++ test/cli_test.exs
@@ -0,0 +1,10 @@
+defmodule CLITest do
+  use ExUnit.Case
+  doctest
+
+  import Issues.CLI, only: [ parse_args: 1]
+
+  test ":help returned for -h and --help options" do
+    assert parse_args(["-h", "anything"]) == :help
+  end
+end
