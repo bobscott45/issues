e9602c923e83dc975fcbc47e959be9b5422bb3ff
robert <bob@rwscott.co.uk>
2022-09-22 11:18:03 +0100

    Refactor Issues.CLI

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -14,14 +14,28 @@ defmodule Issues.CLI do
   Returns `{user, project, count}` or `:help`
 
   """
-   def parse_args(argv) do
-    args = OptionParser.parse(argv, switches: [ help: :boolean], aliases: [h: :help ])
+  def parse_args(argv) do
+    IO.write "argv:"
+    IO.inspect argv
+    OptionParser.parse(argv, switches: [ help: :boolean], aliases: [h: :help ])
+    |> elem(1)
+    |> validate_args
+    |> format_args
+  end
 
-    case args do
-      { [help: true], _, _} -> :help
-      { _, [ user, project, count ], _ } when is_integer(count) -> { user, project, count }
-      { _, [ user, project ], _ } -> { user, project, @default_count }
-      _ -> :help
+  def validate_args([_, _] = args), do: args
+  def validate_args([_,_,count] = args) when is_integer(count), do: args
+  def validate_args([_,_,count] = args) do
+    case Integer.parse(count) do
+      {int_val, _} -> args
+      _ -> false
     end
   end
+  def validate_args(_), do: false
+  def format_args([ user, project, count]) when is_integer(count), do: { user, project, count }
+  def format_args([ user, project, count ]) when is_binary(count), do: { user, project, String.to_integer(count)}
+  def format_args([ user, project ]), do: { user, project, @default_count }
+  def format_args(_), do: :help
+
+
 end
--- test/cli_test.exs
+++ test/cli_test.exs
@@ -13,6 +13,14 @@ defmodule CLITest do
     assert parse_args(["user", "project", 99]) == {"user", "project", 99}
   end
 
+  test "string count converted to int" do
+    assert parse_args(["user", "project", "22"]) == {"user", "project", 22}
+  end
+
+  test "invalid count" do
+    assert parse_args(["user", "project", "invalid number"]) == :help
+  end
+
   test "count defaults to 4 if two values given" do
     assert parse_args(["user", "project"]) == {"user", "project", 4}
   end
